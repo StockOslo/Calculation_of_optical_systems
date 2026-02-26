6b0a48b7f38bdaad19e00e05243647a45cc1f907
erika <erika@DESKTOP-LVN1991>
2026-02-26 21:45:06 +0300

    Add project files.

---
+++ Calculation_of_optical_systems/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Calculation_of_optical_systems
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
